f20113271d0d81fe4b10dfc153ffff08c3371077
Karl Herrick <me@karlherrick.com>
2019-01-07 21:28:35 -0500

    set shell to src/app.js

---
+++ build/uncompiled-unbundled/src/app.js
@@ -0,0 +1,13 @@
+import"./components/app-drawer/x-postpress-hamburger.js";import"./components/x-postpress.js";import"/node_modules/@polymer/app-layout/app-drawer/app-drawer.js";import"/node_modules/@polymer/app-layout/app-header/app-header.js";import"/node_modules/@polymer/app-layout/app-toolbar/app-toolbar.js";import"/node_modules/@polymer/polymer/lib/elements/custom-style.js";import{LitElement,html}from"/node_modules/@polymer/lit-element/lit-element.js";import styles from"./templates/styles/app.js";const xPostpressApp=class extends LitElement{static get properties(){return{siteTitle:{type:String}}}constructor(){super()}firstUpdated(){const drawer=this.shadowRoot.querySelector("app-drawer");this.shadowRoot.querySelector("x-postpress-hamburger").onclick=()=>{drawer.style.display="inherit";drawer.toggle()}}render(){return html`
+      ${styles}
+      <app-header reveals>
+        <app-toolbar>
+          <div id="toolbar-child">
+            <x-postpress-hamburger></x-postpress-hamburger>
+            <div main-title><a href=${this.siteUrl}>${this.siteTitle}</a></div>
+          </div>
+        </app-toolbar>
+      </app-header>
+      <app-drawer swipe-open><slot name="app-drawer-children"></slot></app-drawer>
+      <slot name="x-postpress"></slot>
+    `}};window.customElements.define("x-postpress-app",xPostpressApp);
